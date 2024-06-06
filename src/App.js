bd549364ecbd7e459dac19a1a61490ae00cf52cb
Rashvin <134588990+RashvinSky@users.noreply.github.com>
2024-06-06 12:11:23 +0530

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./Pages/Dashboard";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
 
 function App() {
-  return <div>Hello Word</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
